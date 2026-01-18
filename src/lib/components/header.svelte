813a75660486ac934ee67b428f04beba0f8f84c8
BrunoVieira003 <brunosilverio01@gmail.com>
2026-01-18 19:01:54 -0300

    feat: add links to artists and home page on header

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -1,3 +1,7 @@
-<div class="flex bg-gray-800 py-3 px-40 mb-6">
-    <h1 class="text-2xl">Groov</h1>
+<div class="flex items-center gap-20 bg-gray-800 py-3 px-40 mb-6">
+    <h1 class="text-2xl font-medium">Groov</h1>
+    <div class="flex gap-8 items-center justify-between text-sm *:p-2 *:rounded-lg *:hover:bg-slate-700">
+        <a href="/">Home</a>
+        <a href="/artists">Artists</a>
+    </div>
 </div>
