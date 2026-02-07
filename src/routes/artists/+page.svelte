dfe87f63781ccddecdb72a92fafd3036f16db59d
BrunoVieira003 <brunosilverio01@gmail.com>
2026-02-07 03:51:18 -0300

    feat: add queue and fallback image to playing page

--- src/routes/artists/+page.svelte
+++ src/routes/artists/+page.svelte
@@ -9,7 +9,7 @@
 <div class="grid gap-8 grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6">
     {#each data.artists as artist}
         <div class="flex flex-col items-center justify-between p-4 gap-6 bg-slate-900 rounded-xl">
-            <img class="size-10/12 rounded-full aspect-square" src={EmptyArtist} alt="artist_cover">
+            <img class="size-10/12 rounded-full aspect-square" src={`https://robohash.org/${artist.name}`} alt="artist_cover">
             <a class="text-nowrap hover:underline" href="/artists/{artist.id}">{artist.name}</a>
         </div>
     {/each}
--- src/routes/playing/+page.svelte
+++ src/routes/playing/+page.svelte
@@ -1,8 +1,12 @@
 <script lang="ts">
     import { PUBLIC_API_URL } from "$env/static/public";
+    import SongItemList from "$lib/components/song-item-list.svelte";
+    import SongItem from "$lib/components/song-item.svelte";
     import { currentSong } from "$lib/stores/currentSong";
+    import { songQueue } from "$lib/stores/queue";
+    import emptyImage from '$lib/assets/images/empty.png'
 
-    
+    let coverImage: HTMLImageElement
 
     let coverArtURL = $derived(() => {
         if(!$currentSong){
@@ -12,12 +16,23 @@
         return `${PUBLIC_API_URL}/songs/${$currentSong.id}/cover`
     })
 
+    function playItem(songId?: string){
+        const songIndex = $songQueue.tracks.findIndex(s => s.id === songId)
+        songQueue.playQueue($songQueue.tracks, songIndex)
+    }
+
 </script>
 
 
 {#if $currentSong}
 <div class="flex flex-col gap-4 w-fit items-stretch mx-auto">
-    <img src={coverArtURL()} alt="cover_art" class="aspect-square! self-center size-120 rounded-xl">
+    <img
+    bind:this={coverImage}
+    src={coverArtURL()}
+    alt="cover_art"
+    class="aspect-square! self-center size-120 rounded-xl"
+    onerror={() => coverImage.src = emptyImage}
+    >
     <div class="flex items-center justify-between mx-2">
         <div>
             <p class="text-lg font-bold">{$currentSong?.title}</p>
@@ -31,5 +46,13 @@
             <p>{$currentSong?.year}</p>
         </div>
     </div>
+</div>
+<div class="mt-20 w-11/12 mx-auto">
+    <h1 class="text-xl mb-4">Queue</h1>
+    <SongItemList>
+        {#each $songQueue.tracks as track}
+            <SongItem song={track} onPlayClick={playItem}/>
+        {/each}
+    </SongItemList>
 </div>
 {/if}
