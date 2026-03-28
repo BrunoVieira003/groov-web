d5c0e774c17ecce9221b550d510f2bf320189c7f
BrunoVieira003 <brunosilverio01@gmail.com>
2026-03-28 17:38:27 -0300

    feat: change amount of songs and albums for titles

--- src/routes/artists/[artistId]/+page.svelte
+++ src/routes/artists/[artistId]/+page.svelte
@@ -17,18 +17,20 @@
 </script>
 
 <div class="w-10/12 mx-auto">
-    <div class="mb-6">
+    <div class="mb-4">
         <p class="mb-1 text-sm">Artist</p>
         <h1 class="text-5xl mb-6">{data.artist?.name}</h1>
-        <p class="text-sm">{data.artist?.songs.length} {data.artist && data.artist?.songs.length > 1 ? 'songs': 'song'}</p>
+        <h2 class="text-2xl">Songs</h2>
     </div>
+    
     <SongItemList collectionId={data.artist?.id || ''} collectionType='artist'>
         {#each data.artist?.songs as songItem (songItem.id)}
             <SongItem song={songItem} onPlayClick={playItem}/>
         {/each}
     </SongItemList>
+
     {#if data.artist?.albums && data.artist?.albums.length > 0}
-        <p class="mt-8">{data.artist.albums.length} {data.artist.albums.length > 1 ? 'albums': 'album'}</p>
+        <h2 class="text-2xl">Albums</h2>
         <ArtistAlbums albums={data.artist.albums}/>
     {/if}
 </div>
