15ad90ae441c44fc9230c04844518c8e45c99545
BrunoVieira003 <brunosilverio01@gmail.com>
2026-04-03 15:43:10 -0300

    fix: album on artist page was duplicated

--- src/lib/components/artist-albums.svelte
+++ src/lib/components/artist-albums.svelte
@@ -26,27 +26,5 @@
             <a href="/albums/{album.id}" class="hover:underline text-nowrap text-md w-fit mx-auto font-bold">{album.title}</a>
             <p class="text-sm">{album.year}</p>
         </div>
-        <div class="flex flex-col justify-center gap-4 p-4">
-            <img
-            bind:this={coverImage}
-            src="{PUBLIC_API_URL}/albums/{album.id}/cover"
-            alt="album_cover_art"
-            class="aspect-square! self-center rounded-xl object-cover"
-            onerror={() => coverImage.src = emptyImage }
-            >
-            <a href="/albums/{album.id}" class="hover:underline text-nowrap text-md w-fit mx-auto font-bold">{album.title}</a>
-            <p class="text-sm">{album.year}</p>
-        </div>
-        <div class="flex flex-col justify-center gap-4 p-4">
-            <img
-            bind:this={coverImage}
-            src="{PUBLIC_API_URL}/albums/{album.id}/cover"
-            alt="album_cover_art"
-            class="aspect-square! self-center rounded-xl object-cover"
-            onerror={() => coverImage.src = emptyImage }
-            >
-            <a href="/albums/{album.id}" class="hover:underline text-nowrap text-md w-fit mx-auto font-bold">{album.title}</a>
-            <p class="text-sm">{album.year}</p>
-        </div>
     {/each}
 </div>
