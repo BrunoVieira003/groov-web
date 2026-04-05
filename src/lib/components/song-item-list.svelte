fc78125c6f3de8fb0711f34802a2bf99043b2d53
BrunoVieira003 <brunosilverio01@gmail.com>
2026-04-04 23:36:40 -0300

    fix: song list headers were not aligned with item

--- src/lib/components/song-item-list.svelte
+++ src/lib/components/song-item-list.svelte
@@ -13,7 +13,7 @@
 </script>
 
 <div class="relative flex flex-col w-full">
-    <div class="sticky w-full -top-6 bg-gray-950 grid grid-cols-1 md:grid-cols-2 items-center justify-start gap-10 p-4">
+    <div class="sticky w-full -top-6 bg-gray-950 grid grid-cols-1 md:grid-cols-2 items-center justify-start gap-2 p-4">
         <p class="font-bold text-sm opacity-80">Title</p>
         <p class="hidden md:block font-bold text-sm opacity-80">Album</p>
     </div>
