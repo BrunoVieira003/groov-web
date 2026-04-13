4736584b555e9ffde323eef553c4703cf110564e
BrunoVieira003 <brunosilverio01@gmail.com>
2026-04-12 23:07:14 -0300

    feat: create cassete component
    Cassete component will be used in future alternative layouts

---
+++ src/lib/components/cassete.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+    import { PUBLIC_API_URL } from "$env/static/public";
+    import type { Album } from "$lib/types/album";
+    import Marquee from "./marquee.svelte";
+    import emptyImage from '$lib/assets/images/empty.png'
+
+    interface PropsType{
+        title?: string
+        coverImageSrc?: string
+    }
+
+    let {title = '', coverImageSrc = emptyImage}: PropsType = $props()
+
+    // svelte-ignore non_reactive_update
+    let coverImage: HTMLImageElement
+
+</script>
+<div class="flex flex-col items-center justify-between pt-4 bg-neutral-medium w-full aspect-video rounded-md">
+    <div class="flex flex-col items-center justify-center bg-highlight w-6/7 h-2/3 rounded-md overflow-hidden">
+        <div class="h-full min-h-10 w-full flex justify-center overflow-hidden px-2 py-3">
+            <img
+            bind:this={coverImage}
+            src={coverImageSrc}
+            alt="album_cover_art"
+            class="w-full rounded-xl object-cover"
+            onerror={() => coverImage.src = emptyImage}
+            >
+        </div>
+
+        <div class="w-8/12 h-2/4 flex items-center justify-evenly bg-neutral-light p-2 rounded-sm">
+            <div class="w-1/5 bg-neutral-dark aspect-square rounded-full relative border-4 border-highlight">
+                <div class="absolute left-1/2 -translate-x-1/2 h-full w-1/10 mx-auto bg-highlight"></div>
+                <div class="absolute left-1/2 -translate-x-1/2 h-full w-1/10 mx-auto bg-highlight rotate-45"></div>
+                <div class="absolute left-1/2 -translate-x-1/2 h-full w-1/10 mx-auto bg-highlight rotate-90"></div>
+                <div class="absolute left-1/2 -translate-x-1/2 h-full w-1/10 mx-auto bg-highlight rotate-135"></div>
+                <div class="absolute w-9/12 h-9/12 right-1/2 translate-x-1/2 -translate-y-1/2 top-1/2 bg-neutral-dark rounded-full"></div>
+            </div>
+            <div class="w-2/5 bg-neutral-dark h-2/3 glass"></div>
+            <div class="w-1/5 bg-neutral-dark aspect-square rounded-full relative border-4 border-highlight">
+                <div class="absolute left-1/2 -translate-x-1/2 h-full w-1/10 mx-auto bg-highlight"></div>
+                <div class="absolute left-1/2 -translate-x-1/2 h-full w-1/10 mx-auto bg-highlight rotate-45"></div>
+                <div class="absolute left-1/2 -translate-x-1/2 h-full w-1/10 mx-auto bg-highlight rotate-90"></div>
+                <div class="absolute left-1/2 -translate-x-1/2 h-full w-1/10 mx-auto bg-highlight rotate-135"></div>
+                <div class="absolute w-9/12 h-9/12 right-1/2 translate-x-1/2 -translate-y-1/2 top-1/2 bg-neutral-dark rounded-full"></div>
+            </div>
+        </div>
+
+        <div class="h-full w-fit flex px-2 py-1 items-start justify-start">
+            <Marquee>
+                <p class="text-neutral-darker font-semibold">{title}</p>
+            </Marquee>
+        </div>
+    </div>
+
+    <div class="h-1/4 bg-neutral-light w-4/6 rounded-t-md cassete-shadow"></div>
+</div>
