102e303cddafe4a32ff18b05737b99179ec4baeb
BrunoVieira003 <brunosilverio01@gmail.com>
2026-05-08 23:56:55 -0300

    feat: aggregate frequency bins and fix canvas scaling
    Group FFT bins into averaged bars (avgAmount=8) and limit bars to audible range (under 16K Hz)

--- src/lib/components/visualizers/bar-visualizer.svelte
+++ src/lib/components/visualizers/bar-visualizer.svelte
@@ -32,37 +32,49 @@
         const dpr = window.devicePixelRatio || 1
         
         const rect = canvas.getBoundingClientRect();
-        canvas.width = rect.width * dpr;
-        canvas.height = rect.height * dpr;
+        canvas.width = rect.width * dpr
+        canvas.height = rect.height * dpr
         ctx.scale(dpr, dpr)
 
-        const bufferLength = analyser.frequencyBinCount;
-        const dataArray = new Uint8Array(bufferLength);
-        analyser.getByteFrequencyData(dataArray);
+        const bufferLength = analyser.frequencyBinCount
+        const dataArray = new Uint8Array(bufferLength)
+        analyser.getByteFrequencyData(dataArray)
         
-        animationId = requestAnimationFrame(draw);
+        animationId = requestAnimationFrame(draw)
 
-        const width = canvas.width;
-        const height = canvas.height;
-
+        const width = canvas.width
+        const height = canvas.height
+        console.log('width', width)
+        
         ctx.clearRect(0, 0, width, height)
-
-        const barWidth = (width / bufferLength) * 2.5;
-        let barHeight;
-        let x = 0;
-
-        for (let i = 0; i < bufferLength; i++) {
-        barHeight = (dataArray[i] / 255) * height; // 0-255
-
-        ctx.fillStyle = $currentSong?.color ?? '#fff';
         
-        ctx.fillRect(x, height - barHeight, barWidth, barHeight);
+        const avgAmount = 8
+        const maxIndex = Math.floor((16000 * analyser.fftSize) / analyser.context.sampleRate)
+        const totalBars = Math.floor(maxIndex / avgAmount)
+        const barWidth = (width / totalBars ) - 1
+        let barHeight
 
-        x += barWidth + 1; 
+        let x = 0;
+        for (let i = 0; i < totalBars; i++) {
+
+            let total = 0
+            for (let j = 0; j < avgAmount; j++){
+                total += dataArray[(i * avgAmount) + j]
+            }
+
+            const barValue = total / avgAmount
+
+            barHeight = (barValue / 255) * height; // 0-255
+
+            ctx.fillStyle = $currentSong?.color ?? '#f0f0f0'
+            
+            ctx.fillRect(x, height - barHeight, barWidth, barHeight)
+
+            x += barWidth + 1
         }
     }
 </script>
 
 
-<canvas bind:this={canvas} class="size-full">
+<canvas bind:this={canvas} class="size-full overflow-x-scroll">
 </canvas>
