f7412cb711d057304002b359c1df49429b926443
BrunoVieira003 <brunosilverio01@gmail.com>
2026-06-05 00:06:12 -0300

    feat: divide artist page with song and album tabs

---
+++ src/lib/components/tabs.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+    import type { Snippet } from "svelte";
+
+    interface PropsType{
+        currentTab?: string
+        tabs: string[]
+        children: Snippet
+    }
+
+    let {currentTab = $bindable(), tabs, children}: PropsType = $props()
+</script>
+
+<div class="flex flex-col w-full justify-stretch">
+    <div class="flex w-full justify-stretch items-stretch h-min z-10">
+        {#each tabs as tab}
+            <button 
+            class="text-lg text-subheading text-center p-2 border-b-2 border-neutral-medium data-[active=true]:border-legend data-[active=true]:text-heading"
+            data-active={tab === currentTab}
+            onclick={() => {currentTab = tab}}
+            >
+                {tab}
+            </button>
+        {/each}
+        <span class="flex-1 p-2 border-b-2 border-neutral-medium"></span>
+    </div>
+    <div>
+        {@render children()}
+    </div>
+</div>
--- src/routes/artists/[artistId]/+page.svelte
+++ src/routes/artists/[artistId]/+page.svelte
@@ -1,25 +1,33 @@
 <script lang="ts">
     import ArtistAlbums from "$lib/components/artist-albums.svelte";
     import SongList from "$lib/components/song-list.svelte";
+    import Tabs from "$lib/components/tabs.svelte";
     import type { PageProps } from "./$types";
 
     let { data }: PageProps = $props()
+
+    let currentTab = $state("Songs")
 </script>
 
 <div class="w-11/12 sm:w-10/12 mx-auto">
     <div class="mb-4 text-center sm:text-start">
         <p class="mb-1 text-sm text-legend">Artist</p>
         <h1 class="text-5xl mb-6 text-highlight">{data.artist?.name}</h1>
-        <h2 class="text-2xl text-heading">Songs</h2>
     </div>
 
-    <SongList 
-    collection={ {id: data.artist?.id, type: 'artist', name: data.artist?.name || ''} }
-    tracks={data.artist?.songs || []}
-    />
+    <Tabs tabs={["Songs", "Albums"]} bind:currentTab>
+        {#if currentTab === 'Songs'}
+            <SongList 
+            collection={ {id: data.artist?.id, type: 'artist', name: data.artist?.name || ''} }
+            tracks={data.artist?.songs || []}
+            />
+        {:else if currentTab === 'Albums'}
+            {#if data.artist?.albums && data.artist?.albums.length > 0}
+                <ArtistAlbums albums={data.artist.albums}/>
+            {/if}
+        {/if}
+    </Tabs>
 
-    {#if data.artist?.albums && data.artist?.albums.length > 0}
-        <h2 class="text-2xl mt-6 mb-2 text-heading">Albums</h2>
-        <ArtistAlbums albums={data.artist.albums}/>
-    {/if}
+    
+
 </div>
