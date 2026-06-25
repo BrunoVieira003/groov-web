e0c15ff680ee7a0f510d4d0e605a55762ba9c2a5
BrunoVieira003 <brunosilverio01@gmail.com>
2026-06-25 17:52:33 -0300

    feat: add context menu with Go to Artist option to album item

--- src/lib/components/album/DefaultAlbum.svelte
+++ src/lib/components/album/DefaultAlbum.svelte
@@ -3,15 +3,32 @@
     import type { AlbumProps } from "./album-props";
     import emptyImage from "$lib/assets/images/empty.png";
     import Marquee from "../marquee.svelte";
+    import ContextMenu from "../context-menu/context-menu.svelte";
+    import ContextMenuButton from "../context-menu/context-menu-button.svelte";
+    import { goto } from "$app/navigation";
+    import ArtistIcon from "$lib/assets/icons/artist.svg?raw"
 
     let { album }: AlbumProps = $props();
 
     let coverImage: HTMLImageElement;
+    let contextMenu = $state<ContextMenu>()
+
+    function goToArtist(){
+        goto(`/artists/${album.artist.id}`)
+    }
 </script>
+
+<ContextMenu bind:this={contextMenu} title="Options">
+    <ContextMenuButton onclick={goToArtist} class="flex items-center gap-2">
+        <div class="size-7">{@html ArtistIcon}</div>
+        Go to artist
+    </ContextMenuButton>
+</ContextMenu>
 
 <a
     class="space-y-4 hover:bg-neutral-medium p-4 rounded-md"
     href="/albums/{album.id}"
+    oncontextmenu={contextMenu.show}
 >
     <img
         bind:this={coverImage}
