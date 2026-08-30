3ec605ae80a00167517c627e7fb81f1014479dc9
BrunoVieira003 <brunosilverio01@gmail.com>
2026-08-30 17:16:20 -0300

    refactor: moves context menu logic to item

--- src/lib/components/song-item.svelte
+++ src/lib/components/song-item.svelte
@@ -1,29 +1,33 @@
 <script lang="ts">
     import {
         getCollection,
-        type CollectionType,
     } from "$lib/contexts/collection-context";
     import { fallbackImage } from "$lib/plugins/fallbackImage";
     import {
         currentSong,
-        currentTime,
         paused,
-        togglePlay,
     } from "$lib/stores/player";
-    import { targetedSong, targetedTrackNumber } from "$lib/stores/songAction";
+    import { targetedSong } from "$lib/stores/songAction";
     import type Song from "$lib/types/song";
     import { fly } from "svelte/transition";
     import PlayButton from "./player/buttons/play-button.svelte";
     import Marquee from "./marquee.svelte";
     import ArtistsLabel from "./artists-label.svelte";
     import type { DragEventHandler, MouseEventHandler } from "svelte/elements";
+    import ContextMenu from "./context-menu/context-menu.svelte";
+    import ContextMenuButton from "./context-menu/context-menu-button.svelte";
+    import ContextMenuSubmenu from "./context-menu/context-menu-submenu.svelte";
+    import PlaylistSelect from "./forms/playlist-select.svelte";
+    import ContextMenuDivider from "./context-menu/context-menu-divider.svelte";
+    import { songQueue } from "$lib/stores/queue";
+    import toast from "svelte-hot-french-toast";
+    import { invalidateAll } from "$app/navigation";
 
     interface PropsType {
         song: Song;
         trackNumber: number;
         index: number;
         onPlay: MouseEventHandler<HTMLButtonElement> | null | undefined;
-        onContextMenu?: MouseEventHandler<Element>;
         draggable?: boolean;
         onDragStart?: DragEventHandler<HTMLDivElement> | null | undefined;
         onDragEnd?: DragEventHandler<HTMLDivElement> | null | undefined;
@@ -35,7 +39,6 @@
         trackNumber,
         index,
         onPlay,
-        onContextMenu,
         draggable = false,
         onDragStart,
         onDragOver,
@@ -43,6 +46,65 @@
     }: PropsType = $props();
 
     let windowWidth = $state<number>(10000);
+    let contextMenu = $state<ContextMenu>()
+
+    let isOnQueue = $derived($songQueue.tracks.includes($targetedSong));
+
+    function addToQueue() {
+        songQueue.addToQueue($targetedSong);
+        contextMenu?.hide();
+        toast.success("Song added to queue");
+    }
+
+    function removeFromQueue() {
+        songQueue.removeFromQueue(index);
+        contextMenu?.hide();
+        toast.success("Song removed from queue");
+    }
+
+    function playNextQueue() {
+        songQueue.playNext($targetedSong);
+        contextMenu?.hide();
+        toast.success("Song added to queue");
+    }
+
+    function addToPlaylist(playlistId: string) {
+        fetch(`/api/playlists/${playlistId}/song`, {
+            method: "POST",
+            body: JSON.stringify({ songId: $targetedSong.id }),
+        })
+            .then(() => {
+                toast.success("Song added to playlist");
+            })
+            .catch(() => {
+                toast.error("Failed to add song to playlist");
+            })
+            .finally(contextMenu?.hide);
+    }
+
+    function removeFromPlaylist(playlistId: string | undefined) {
+        if (!playlistId) return;
+
+        fetch(`/api/playlists/${playlistId}/song`, {
+            method: "DELETE",
+            body: JSON.stringify({ relationId: $targetedSong?.relationId }),
+        })
+            .then(() => {
+                toast.success("Song removed from playlist");
+            })
+            .catch(() => {
+                toast.error("Failed to remove song from playlist");
+            })
+            .finally(async () => {
+                contextMenu?.hide();
+                await invalidateAll();
+            });
+    }
+
+    function openContextMenu(e: MouseEvent){
+        targetedSong.set(song)
+        contextMenu?.show(e)
+    }
 
     const collection = getCollection();
 </script>
@@ -53,7 +115,7 @@
     class="grid grid-cols-1 md:grid-cols-[3.5ch_1fr_1fr] items-center justify-start gap-2 p-4 text-subheading hover:bg-bg-hover data-[active=true]:bg-bg-active data-[active=true]:text-heading"
     data-active={song.id === $currentSong?.id}
     style="--colorful: {$currentSong?.color};"
-    oncontextmenu={onContextMenu}
+    oncontextmenu={openContextMenu}
     {draggable}
     data-index={index}
     ondragstart={onDragStart}
@@ -102,3 +164,27 @@
         <ArtistsLabel artists={song.authors} size="default" />
     {/if}
 </div>
+
+<ContextMenu bind:this={contextMenu}>
+    <div class="flex flex-col">
+        <ContextMenuButton onclick={playNextQueue}>Play next</ContextMenuButton>
+        <ContextMenuButton onclick={addToQueue}
+            >Add to the queue</ContextMenuButton
+        >
+        {#if isOnQueue}
+            <ContextMenuButton onclick={removeFromQueue}
+                >Remove from queue</ContextMenuButton
+            >
+        {/if}
+
+        <ContextMenuSubmenu label="Add to playlist">
+            <PlaylistSelect onPick={addToPlaylist} />
+        </ContextMenuSubmenu>
+        {#if collection.collection.type === "playlist"}
+            <ContextMenuDivider title="Playlist actions" />
+            <ContextMenuButton onclick={() => removeFromPlaylist(collection.collection.id)}
+                >Remove from playlist</ContextMenuButton
+            >
+        {/if}
+    </div>
+</ContextMenu>
--- src/lib/components/song-list.svelte
+++ src/lib/components/song-list.svelte
@@ -1,19 +1,13 @@
 <script lang="ts">
     import type Song from "$lib/types/song";
-    import ContextMenu from "./context-menu/context-menu.svelte";
-    import PlaylistSelect from "./forms/playlist-select.svelte";
     import { songQueue, type Collection } from "$lib/stores/queue";
     import toast from "svelte-hot-french-toast";
     import { invalidateAll } from "$app/navigation";
-    import { targetedSong, targetedTrackNumber } from "$lib/stores/songAction";
     import {
         currentSong,
         currentTime,
         togglePlay,
     } from "$lib/stores/player";
-    import ContextMenuButton from "./context-menu/context-menu-button.svelte";
-    import ContextMenuSubmenu from "./context-menu/context-menu-submenu.svelte";
-    import ContextMenuDivider from "./context-menu/context-menu-divider.svelte";
     import SongItem from "./song-item.svelte";
     import { setCollection } from "$lib/contexts/collection-context";
 
@@ -38,64 +32,10 @@
         setCollection({ collection, tracks });
     }
 
-
-    let contextMenu = $state<ContextMenu>();
-    let isOnQueue = $derived($songQueue.tracks.includes($targetedSong));
     let windowWidth = $state<number>(10000);
 
     let dragFrom = $state<number | null>(null);
     let dragTo = $state<number | null>(null);
-
-    function addToPlaylist(playlistId: string) {
-        fetch(`/api/playlists/${playlistId}/song`, {
-            method: "POST",
-            body: JSON.stringify({ songId: $targetedSong.id }),
-        })
-            .then(() => {
-                toast.success("Song added to playlist");
-            })
-            .catch(() => {
-                toast.error("Failed to add song to playlist");
-            })
-            .finally(contextMenu?.hide);
-    }
-
-    function removeFromPlaylist(playlistId: string | undefined) {
-        if (!playlistId) return;
-
-        fetch(`/api/playlists/${playlistId}/song`, {
-            method: "DELETE",
-            body: JSON.stringify({ relationId: $targetedSong?.relationId }),
-        })
-            .then(() => {
-                toast.success("Song removed from playlist");
-            })
-            .catch(() => {
-                toast.error("Failed to remove song from playlist");
-            })
-            .finally(async () => {
-                contextMenu?.hide();
-                await invalidateAll();
-            });
-    }
-
-    function addToQueue() {
-        songQueue.addToQueue($targetedSong);
-        contextMenu?.hide();
-        toast.success("Song added to queue");
-    }
-
-    function removeFromQueue() {
-        songQueue.removeFromQueue($targetedTrackNumber);
-        contextMenu?.hide();
-        toast.success("Song removed from queue");
-    }
-
-    function playNextQueue() {
-        songQueue.playNext($targetedSong);
-        contextMenu?.hide();
-        toast.success("Song added to queue");
-    }
 
     function playItem(song: Song) {
         if (song.id === $currentSong?.id) {
@@ -169,18 +109,13 @@
             <p class="hidden md:block font-bold text-sm">Artists</p>
         {/if}
     </div>
-    {#each tracks as song, index (collection?.type === "playlist" ? song.relationId : song)}
+    {#each tracks as song, index (index)}
         <hr class="text-divider">
         <SongItem
             {song}
             {index}
             trackNumber={song.trackNumber ?? index + 1}
             onPlay={() => playItem(song)}
-            onContextMenu={(e) => {
-                targetedSong.set(song);
-                targetedTrackNumber.set(song.trackNumber ?? index + 1);
-                contextMenu && contextMenu.show(e);
-            }}
             draggable={collection?.type === "playlist"}
             onDragStart={dragStart}
             onDragOver={dragOver}
@@ -188,27 +123,3 @@
         />
     {/each}
 </div>
-
-<ContextMenu bind:this={contextMenu}>
-    <div class="flex flex-col">
-        <ContextMenuButton onclick={playNextQueue}>Play next</ContextMenuButton>
-        <ContextMenuButton onclick={addToQueue}
-            >Add to the queue</ContextMenuButton
-        >
-        {#if isOnQueue}
-            <ContextMenuButton onclick={removeFromQueue}
-                >Remove from queue</ContextMenuButton
-            >
-        {/if}
-
-        <ContextMenuSubmenu label="Add to playlist">
-            <PlaylistSelect onPick={addToPlaylist} />
-        </ContextMenuSubmenu>
-        {#if collection?.type === "playlist"}
-            <ContextMenuDivider title="Playlist actions" />
-            <ContextMenuButton onclick={() => removeFromPlaylist(collection.id)}
-                >Remove from playlist</ContextMenuButton
-            >
-        {/if}
-    </div>
-</ContextMenu>
